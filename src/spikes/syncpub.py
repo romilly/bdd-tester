be4765bc0fc22da3cb35005328b2d7ebea4f04cc
Romilly Cocking <romilly.cocking@gmail.com>
2018-06-14 18:00:10 +0100

    Added microbit classes

--- src/spikes/syncpub.py
+++ src/spikes/syncpub.py
@@ -25,14 +25,22 @@ class Publisher(Qber):
         subscribers = 0
         while subscribers < SUBSCRIBERS_EXPECTED:
             # wait for synchronization request
-            msg = self.syncservice.recv()
-            print('got sync')
+            msg = self.sync_recv()
             # send synchronization reply
-            self.syncservice.send_string('%i' % subscribers)
+            self.sync_send('%i' % subscribers)
             subscribers += 1
             print("+1 subscriber (%i/%i)" % (subscribers, SUBSCRIBERS_EXPECTED))
-        self.send(self.publisher, 'button_a')
-        self.send(self.publisher, 'END')
+        self.publish('button_a')
+        self.publish('END')
+
+    def sync_send(self, message):
+        self.send(self.syncservice, message)
+
+    def sync_recv(self):
+        return self.recv(self.syncservice)
+
+    def publish(self, message):
+        self.send(self.publisher, message)
 
     def bind_publisher(self, port):
         self.publisher = self.context.socket(zmq.PUB)

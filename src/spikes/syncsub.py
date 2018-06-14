47125ce8e68719422ceac3cb98768dff74ed528c
Romilly Cocking <romilly.cocking@gmail.com>
2018-06-14 13:55:17 +0100

    Some refactoring

--- src/spikes/syncsub.py
+++ src/spikes/syncsub.py
@@ -8,36 +8,58 @@ import zmq
 class Subscriber():
     def __init__(self):
         self.context = zmq.Context()
+        self.subsock = None
+
+    def run(self):
 
         # First, connect our subscriber socket
-        subscriber = self.context.socket(zmq.SUB)
-        subscriber.connect('tcp://localhost:5561')
-        subscriber.setsockopt(zmq.SUBSCRIBE, b'')
+        self.subscribe(5561)
 
         time.sleep(1)
 
         # Second, synchronize with publisher
-        syncclient = self.context.socket(zmq.REQ)
-        syncclient.connect('tcp://localhost:5562')
+        self.sync()
 
-        # send a synchronization request
-        syncclient.send_string('')
-
-        # wait for synchronization reply
-        id = syncclient.recv().decode('utf8')
-        print('my id is %i' % int(id))
 
 
         # Third, get our updates and report how many we got
         nbr = 0
         while True:
-            msg = subscriber.recv().decode('utf8')
+            msg = self.sub_recv()
             if msg == 'END':
                 break
             nbr += 1
 
         print ('Received %d updates' % nbr)
 
+    def sub_recv(self):
+        return self.recv(self.subsock)
+
+    def sync(self):
+        self.syncclient = self.context.socket(zmq.REQ)
+        self.syncclient.connect('tcp://localhost:5562')
+
+        # send a synchronization request
+        self.sync_send('')
+
+        # wait for synchronization reply
+        id = self.sync_recv()
+        print('my id is %i' % int(id))
+
+    def recv(self, socket):
+        return socket.recv().decode('utf8')
+
+    def sync_recv(self):
+        return self.recv(self.syncclient)
+
+    def sync_send(self, message):
+        self.syncclient.send_string(message)
+
+    def subscribe(self, port):
+        self.subsock = self.context.socket(zmq.SUB)
+        self.subsock.connect('tcp://localhost:%i' % port)
+        self.subsock.setsockopt(zmq.SUBSCRIBE, b'')
+
 
 if __name__ == '__main__':
-   Subscriber()
+   Subscriber().run()

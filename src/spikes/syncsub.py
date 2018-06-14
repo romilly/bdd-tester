581c8eb3e064feecfad65305f8801dfe5f64b1bb
Romilly Cocking <romilly.cocking@gmail.com>
2018-06-14 13:42:42 +0100

    Starting to create classes

--- src/spikes/syncsub.py
+++ src/spikes/syncsub.py
@@ -5,38 +5,39 @@ import time
 
 import zmq
 
+class Subscriber():
+    def __init__(self):
+        self.context = zmq.Context()
 
-def main():
-    context = zmq.Context()
+        # First, connect our subscriber socket
+        subscriber = self.context.socket(zmq.SUB)
+        subscriber.connect('tcp://localhost:5561')
+        subscriber.setsockopt(zmq.SUBSCRIBE, b'')
 
-    # First, connect our subscriber socket
-    subscriber = context.socket(zmq.SUB)
-    subscriber.connect('tcp://localhost:5561')
-    subscriber.setsockopt(zmq.SUBSCRIBE, b'')
+        time.sleep(1)
 
-    time.sleep(1)
+        # Second, synchronize with publisher
+        syncclient = self.context.socket(zmq.REQ)
+        syncclient.connect('tcp://localhost:5562')
 
-    # Second, synchronize with publisher
-    syncclient = context.socket(zmq.REQ)
-    syncclient.connect('tcp://localhost:5562')
+        # send a synchronization request
+        syncclient.send_string('')
 
-    # send a synchronization request
-    syncclient.send_string('')
-
-    # wait for synchronization reply
-    id = syncclient.recv().decode('utf8')
-    print('my id is %i' % int(id))
+        # wait for synchronization reply
+        id = syncclient.recv().decode('utf8')
+        print('my id is %i' % int(id))
 
 
-    # Third, get our updates and report how many we got
-    nbr = 0
-    while True:
-        msg = subscriber.recv().decode('utf8')
-        if msg == 'END':
-            break
-        nbr += 1
+        # Third, get our updates and report how many we got
+        nbr = 0
+        while True:
+            msg = subscriber.recv().decode('utf8')
+            if msg == 'END':
+                break
+            nbr += 1
 
-    print ('Received %d updates' % nbr)
+        print ('Received %d updates' % nbr)
+
 
 if __name__ == '__main__':
-    main()
+   Subscriber()

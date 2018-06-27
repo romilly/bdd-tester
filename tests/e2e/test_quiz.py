1a655a352f64e24c5c1528836e916472c282a0e4
Romilly Cocking <romilly.cocking@gmail.com>
2018-06-27 09:54:53 +0100

    Allow a set of events in any orfe.

--- tests/e2e/test_quiz.py
+++ tests/e2e/test_quiz.py
@@ -1,4 +1,6 @@
 import sys
+import time
+
 from time import sleep
 from unittest import TestCase
 from hamcrest import assert_that
@@ -21,20 +23,40 @@ class ControllerTest(TestCase):
         self.controller.press(BUTTON_A, 'QuizRunner')
         event = self.controller.read_event()
         assert_that(event, is_event('display', 'runner','QuizRunner'))
-        event = self.controller.read_event()
-        assert_that(event, is_event('display', 'checking in','Team 1'))
-        event = self.controller.read_event()
-        assert_that(event, is_event('display', 'checking in','Team 2'))
-        sleep(0.1)
+        self.expect_events(is_event('display', 'checking in','Team 1'),is_event('display', 'checking in','Team 2'))
         self.controller.press(BUTTON_B,'Team 1')
         event = self.controller.read_event()
         assert_that(event, is_event('display', 'check','Team 1'))
         event = self.controller.read_event()
         assert_that(event, is_event('display', 'team 1 checked in','QuizRunner'))
-
-
-
+        event = self.controller.read_event()
+        assert_that(event, is_event('display', 'team: 1','Team 1'))
+        sleep(0.1) # otherwise team 2 might get team 1's message after the button press!
+        self.controller.press(BUTTON_B,'Team 2')
+        event = self.controller.read_event()
+        assert_that(event, is_event('display', 'check','Team 2'))
+        event = self.controller.read_event()
+        assert_that(event, is_event('display', 'team 2 checked in','QuizRunner'))
 
 
     def tearDown(self):
         self.controller.close()
+
+    def expect_events(self, *event_matchers, timeout_ms=100):
+        start = time.perf_counter()
+        event_set = set(event_matchers)
+        while len(event_set):
+            if time.perf_counter() - start > 1000.0 * timeout_ms:
+                raise TimeoutError()
+            next_event = self.controller.read_event()
+            if next_event:
+                if self.event_is_ok(event_set, next_event):
+                    continue
+                self.fail('unexpected event %s' % str(next_event))
+
+    def event_is_ok(self, event_set, next_event):
+        for possible in event_set:
+            if possible.matches(next_event):
+                event_set.remove(possible)
+                return True
+        return False

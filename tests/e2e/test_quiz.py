24d9f64cbd5c0657e2619cfd6af4a5e12ebe84e4
Romilly Cocking <romilly.cocking@gmail.com>
2019-02-13 10:12:52 +0000

    Test exposes bug: game misbehaves if QuizMaster ends game ofter a wrong answer.

--- tests/e2e/test_quiz.py
+++ tests/e2e/test_quiz.py
@@ -23,26 +23,26 @@ class QuizRunner(AbstractControllerTest):
     def setUp(self):
         AbstractControllerTest.setUp(self)
 
-    # def test_forced_game_end(self):
-    #     self.start_quiz()
-    #     self.expect(see(QUIZ_RUNNER, 'Play time!'))
-    #     ## play a round
-    #     self.expect(
-    #         see(QUIZ_RUNNER, 'round 1'),
-    #         see(TEAM1, 'round 1'),
-    #         see(TEAM2, 'round 1'))
-    #     self.press(TEAM2, BUTTON_A)
-    #     self.press(QUIZ_RUNNER, BUTTON_B)
-    #     self.expect(
-    #         see(QUIZ_RUNNER, 'team 2 buzzing'),
-    #         see(TEAM2, 'team 2 buzzing'))
-    #     self.press_extra(QUIZ_RUNNER, BUTTON_D)
-    #     self.expect(
-    #         see(QUIZ_RUNNER, 'game over'),
-    #         see(QUIZ_RUNNER, all_of(contains_string("that's all"),
-    #                                 contains_string('1:0'),
-    #                                 contains_string('2:0'))))
-    #     sleep(1.0)  # wait for cool-down
+    def test_forced_game_end(self):
+        self.start_quiz()
+        self.expect(see(QUIZ_RUNNER, 'Play time!'))
+        ## play a round
+        self.expect(
+            see(QUIZ_RUNNER, 'round 1'),
+            see(TEAM1, 'round 1'),
+            see(TEAM2, 'round 1'))
+        self.press(TEAM2, BUTTON_A)
+        self.press(QUIZ_RUNNER, BUTTON_B)
+        self.expect(
+            see(QUIZ_RUNNER, 'team 2 buzzing'),
+            see(TEAM2, 'team 2 buzzing'))
+        self.press_extra(QUIZ_RUNNER, BUTTON_D)
+        self.expect(
+            see(QUIZ_RUNNER, 'game over'),
+            see(QUIZ_RUNNER, all_of(contains_string("that's all"),
+                                    contains_string('1:0'),
+                                    contains_string('2:0'))))
+        sleep(1.0)  # wait for cool-down
 
     def start_quiz(self):
         self.run_scripts(Target(QUIZ_RUNNER, '/home/romilly/git/active/bbc-microbit-nsp/src/quizrunner/quizrunner.py'),
